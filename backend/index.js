6d8207a8a5caf9b24df40ca1b531247a91ff61fd
kaywoon <e1155668@u.nus.edu>
2024-10-12 11:24:32 +0800

    initialize express backend
    install express & nodemon

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mongoose = require('mongoose'); 
+const nodemon = require('nodemon');
+
+const app = express(); 
+
+app.listen(3000, () => { 
+    console.log('Server is running on port 3000')
+});
