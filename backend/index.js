5e625abdb9f74109b0230c3a020f54d198361030
kaywoon <e1155668@u.nus.edu>
2024-10-12 15:59:05 +0800

    add authentication route

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose'); 
 const nodemon = require('nodemon');
-const cors = require('cors'); // Import cors
-const UserModel = require('./models/User');
+const cors = require('cors'); 
 const User = require('./models/User');
 const dotenv = require('dotenv').config(); 
+const router = require('./routes/auth');
 
 const app = express(); 
 app.use(express.json());
@@ -22,38 +22,4 @@ mongoose.connect(process.env.MONGO_URI)
     console.log("Connection failed")
 });
 
-app.post('/register', async (req, res) => { 
-    const {name, email, password} = req.body;
-
-    try { 
-        const existingUser = await User.findOne({ email }); 
-        if (existingUser) {
-            console.log(existingUser)
-            return res.status(400).json({ message: 'User already exists' });
-        }
-
-        const newUser = new User({ name, email, password });
-        await newUser.save();
-        res.status(201).json({ message: 'User created successfully' });
-
-    } catch (error) { 
-        res.status(500).json({ error: error });
-    }
-    
-}); 
-
-app.post('/login', async (req, res) => {
-    const {email, password} = req.body; 
-
-    try {
-        // Check if the user exists
-        const user = await User.findOne({ email });
-        if (!user || user.password !== password) {
-          return res.status(400).json({ message: 'Invalid credentials' });
-        }
-    
-        res.status(200).json({ message: 'Login successful', user });
-      } catch (error) {
-        res.status(500).json({ error: 'Login error' });
-      }
-}); 
+app.use('/api/auth', router);
---
+++ backend/routes/auth.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+router.post('/register', async (req, res) => { 
+    const {name, email, password} = req.body;
+
+    try { 
+        const existingUser = await User.findOne({ email }); 
+        if (existingUser) {
+            console.log(existingUser)
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
+        const newUser = new User({ name, email, password });
+        await newUser.save();
+        res.status(201).json({ message: 'User created successfully' });
+
+    } catch (error) { 
+        res.status(500).json({ error: error });
+    }
+    
+}); 
+
+router.post('/login', async (req, res) => {
+    const {email, password} = req.body; 
+
+    try {
+        // Check if the user exists
+        const user = await User.findOne({ email });
+        if (!user || user.password !== password) {
+          return res.status(400).json({ message: 'Invalid credentials' });
+        }
+    
+        res.status(200).json({ message: 'Login successful', user });
+      } catch (error) {
+        res.status(500).json({ error: 'Login error' });
+      }
+}); 
+
+module.exports = router;
