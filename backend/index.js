25016bd8929310f0cb2ac90002a6b613611066fd
kaywoon <e1155668@u.nus.edu>
2024-10-12 12:18:04 +0800

    create user model

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose'); 
 const nodemon = require('nodemon');
+const UserModel = require('./models/User')
 
 const app = express(); 
 
