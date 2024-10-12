ca55015e4d3d43aa842c915efd5b9ea92de31dac
kaywoon <e1155668@u.nus.edu>
2024-10-12 11:39:05 +0800

    connect to mongodb

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,13 @@ const nodemon = require('nodemon');
 
 const app = express(); 
 
-app.listen(3000, () => { 
-    console.log('Server is running on port 3000')
+mongoose.connect("mongodb+srv://root:PSApassword@psa-db.2csz1.mongodb.net/PSA-DB?retryWrites=true&w=majority&appName=PSA-DB")
+.then(() => {
+    console.log("Connected to database");
+    app.listen(3000, () => { 
+        console.log('Server is running on port 3000');
+    });
+})
+.catch(() => {
+    console.log("Connection failed")
 });
