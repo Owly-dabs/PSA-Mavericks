4d349c1789b45f70385ba298531b2817ae3b7fda
kaywoon <e1155668@u.nus.edu>
2024-10-12 17:55:12 +0800

    create getAllActivities method for engagement page

--- backend/routes/engagement.js
+++ backend/routes/engagement.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Activity = require('../models/Activity');
 
+//create activity 
 router.post('/createActivity', async (req, res) => {
     const { title, details, image, date, time, vacancies, creator, category } = req.body;
   
@@ -14,6 +15,17 @@ router.post('/createActivity', async (req, res) => {
         console.log(error);
         res.status(500).json({ error: 'Error creating activity' });
     }
-  });
+});
+
+//retrieve all activities 
+router.get('/getAllActivities', async (req, res) => {
+    try {
+      const activities = await Activity.find();
+      res.status(200).json(activities);
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching activities' });
+    }
+});
+
 
 module.exports = router;
