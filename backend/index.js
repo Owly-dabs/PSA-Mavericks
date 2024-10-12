dcd42a35615115a095c0f21bcf4feb77ef050bbf
kaywoon <e1155668@u.nus.edu>
2024-10-12 13:00:10 +0800

    update register method
    includes case when user already exists

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const nodemon = require('nodemon');
 const UserModel = require('./models/User')
 
 const app = express(); 
+app.use(express.json());
 
 mongoose.connect("mongodb+srv://root:PSApassword@psa-db.2csz1.mongodb.net/PSA-DB?retryWrites=true&w=majority&appName=PSA-DB")
 .then(() => {
@@ -17,10 +18,20 @@ mongoose.connect("mongodb+srv://root:PSApassword@psa-db.2csz1.mongodb.net/PSA-DB
 });
 
 app.post('/register', (req, res) => { 
-    UserModel.create(req.body)
-    .then(() => {
-        user => res.json(user);
-        console.log("user registered");
-    })
-    .catch(err => res.json(err))
+    const {name, email, password} = req.body;
+
+    try { 
+        const existingUser = User.findOne({ email }); 
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
+        const newUser = new User({ name, email, password });
+        newUser.save();
+        res.status(201).json({ message: 'User created successfully' });
+
+    } catch (error) { 
+        res.status(500).json({ error: 'Error creating user' });
+    }
+    
 }); 
