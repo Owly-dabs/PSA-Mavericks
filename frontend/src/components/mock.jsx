681319c41aaf8bc33f7c6f7727c0c7c05fd033f9
Owly-dabs <owlycodes@gmail.com>
2024-10-13 22:11:59 +0800

    fix mock indexing

--- frontend/src/components/mock.jsx
+++ frontend/src/components/mock.jsx
@@ -1,6 +1,6 @@
 export const projectManagementCourses = [
     {
-      id: 1,
+      id: 0,
       title: "Project Management Professional (PMP)®",
       description: "Prepare for the PMP certification exam with this course, which covers all essential aspects of project management, including the PMBOK® Guide framework and Agile practices.",
       category: "Project Management",
@@ -10,7 +10,7 @@ export const projectManagementCourses = [
       image: "https://tromenzlearning.com/public/assets/img/products/2022-08-11-16602371121505528946.png", 
     },
     {
-      id: 2,
+      id: 1,
       title: "Google Project Management Certificate",
       description: "Offered by Google, this course provides fundamental knowledge on project management tools, methodologies, and practices, including Agile and Scrum.",
       category: "Project Management",
@@ -20,7 +20,7 @@ export const projectManagementCourses = [
       image: "https://i.ytimg.com/vi/vmwjaY13NPA/maxresdefault.jpg", 
     },
     {
-      id: 3,
+      id: 2,
       title: "Agile Project Management Specialization",
       description: "Learn the principles of Agile methodology and how to apply it in project management. This specialization offers hands-on training and practical project management skills.",
       category: "Project Management",
@@ -30,7 +30,7 @@ export const projectManagementCourses = [
       image: "https://prod-discovery.edx-cdn.org/media/course/image/3096d18e-5466-41af-8214-461e1dc25498-1fc8eec23060.small.jpg", 
     },
     {
-      id: 4,
+      id: 3,
       title: "Project Management Principles and Practices",
       description: "This specialization covers the key principles of project management, from initiating and planning to executing and closing projects.",
       category: "Project Management",
@@ -40,7 +40,7 @@ export const projectManagementCourses = [
       image: "https://www.coursya.com/wp-content/uploads/2023/01/project-management-principles-and-practices-specialization.jpeg",
     },
     {
-      id: 5,
+      id: 4,
       title: "Mastering Project Management with PMP®",
       description: "Prepare for the Project Management Professional (PMP) exam with this detailed course that covers advanced project management skills and the latest PMP exam requirements.",
       category: "Project Management",
@@ -50,7 +50,7 @@ export const projectManagementCourses = [
       image: "https://www.mindcypress.com/cdn/shop/articles/Mastering_Project_Management_The_Key_to_PMP_Exam_Success.png?v=1726033841", 
     },
     {
-      id: 6,
+      id: 5,
       title: "Project Management for Beginners: Fundamentals of Project Management",
       description: "Learn the fundamental skills required to successfully manage projects from planning to execution in this introductory project management course.",
       category: "Project Management",
@@ -60,7 +60,7 @@ export const projectManagementCourses = [
       image: "https://img-c.udemycdn.com/course/750x422/1978132_e90d_5.jpg",
     },
     {
-      id: 7,
+      id: 6,
       title: "Certified Associate in Project Management (CAPM)®",
       description: "This course will help you prepare for the CAPM certification exam, covering project management fundamentals and best practices based on the PMBOK® Guide.",
       category: "Project Management",
